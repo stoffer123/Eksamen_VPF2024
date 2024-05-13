dca9631d0108e5e4f826c86b22297179ca12d2a7
stoffer123 <christophermikkelsen@live.dk>
2024-05-13 21:47:44 +0200

    Added namechecking in the init()

--- src/main/java/dk/cphbusiness/vp/f2024/Eksamen/server/commands/Exit.java
+++ src/main/java/dk/cphbusiness/vp/f2024/Eksamen/server/commands/Exit.java
@@ -6,36 +6,29 @@ import dk.cphbusiness.vp.f2024.Eksamen.textio.TextIO;
 
 import java.io.IOException;
 
-public class Exit implements Command {
+public class Exit {
     private final String name;
     private final String description;
     private final ChatServer server;
 
     public Exit(ChatServer server) {
         this.name = "EXIT";
-        this.description = name + " - Exits the program";
+        this.description ="/" + name + " - Exits the program";
         this.server = server;
 
     }
 
-    @Override
-    public void execute(User user, String[] args) throws IOException {
-        if(args.length != 0) {
-            user.sendMessage("Exit command requires no arguments!");
-            return;
-        }
-
+    public void execute(User user) throws IOException {
         user.sendMessage("Shutting down program");
         server.removeUser(user);
         user.close();
+
     }
 
-    @Override
     public String getName() {
         return name;
     }
 
-    @Override
     public String getDescription() {
         return description;
     }
